07b25329a64a7f177cd6af601a777795348df353
fredy <fredy@kali.fredy>
2023-02-23 16:20:04 +0300

    a function that checks for uppercase character.
    Prototype: int _isupper(int c);
    Returns 1 if c is uppercase
    Returns 0 otherwise

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,14 +9,8 @@
 
 int _isupper(int c)
 {
-	int j;
-
-	for (c = 'A'; c <= 'Z'; c++;)
-	{
-		if (j == c)
-			return (1);
-		else
-			return (0);
-
-	}
+	if ((c == 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
