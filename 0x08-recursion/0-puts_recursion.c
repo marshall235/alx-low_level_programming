572f9dc8c57c02a9192660da5b5297e5c4f05fd6
fredy <fredy@kali.fredy>
2023-03-09 12:24:33 +0300

    prints a string, followed by a new line.

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,12 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
+	if (*s == '\0')
 	{
-		_putchar(s);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+
+	_putchar(*s);
+	_putchar_recursion(++s);
 }
