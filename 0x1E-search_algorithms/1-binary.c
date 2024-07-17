006161c00d771c42012f553b84df958f32a85642
marshall235 <fredie1738@gmail.com>
2024-07-17 19:08:34 +0300

    a function that searches for a value in a sorted array of integers using the Binary search algorithm

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,13 +1,13 @@
 #include "search_algos.h"
 
+
 /**
- * binary_recursive - recursive algorithm for binary search
- * @array: pointer to the first position
- * @size: Length of the array
+ * binary_recursive - recusrive algorithm for binary search
+ * @array: pointer to first position
+ * @size: length of array
  * @value: element to search
  * Return: index of value
  */
-
 size_t binary_recursive(int *array, size_t size, int value)
 {
 	size_t mid = size / 2;
@@ -15,25 +15,29 @@ size_t binary_recursive(int *array, size_t size, int value)
 
 	if (!array || size == 0)
 		return (-1);
+
 	printf("Searching in array: ");
 	for (aux = 0; aux < size; aux++)
 		printf("%d%s", array[aux], (aux != size - 1) ? ", " : "\n");
+
 	if (mid && size % 2 == 0)
 		mid--;
 	if (value == array[mid])
 		return (mid);
 	if (value < array[mid])
 		return (binary_recursive(array, mid, value));
+
 	mid++;
 	return (binary_recursive(array + mid, size - mid, value) + mid);
 }
 
+
 /**
- * binary_search - impliment binary search Algo
- * @array: given aray to search in
- * @size: size of the array
- * @value: value to search for
- * Return: index or -1
+ * binary_search - searches for a value in a sorted array
+ * @array: pointer ti first position of array
+ * @size: length of array
+ * @value: value to search
+ * Return: index of value in array
  */
 int binary_search(int *array, size_t size, int value)
 {
@@ -41,7 +45,7 @@ int binary_search(int *array, size_t size, int value)
 
 	el = binary_recursive(array, size, value);
 
-	if (el >= 0 && array(el) != value)
+	if (el >= 0 && array[el] != value)
 		return (-1);
 	return (el);
 }
